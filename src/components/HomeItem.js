b9648234960996fe421216a71896c4c21fc87826
ruthvik <ruthvik93@gmail.com>
2019-12-23 15:30:05 +0530

    Added loader icon in the blogs

--- src/components/HomeItem.js
+++ src/components/HomeItem.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from "react"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
 
 
 function HomeItem({match}){
     console.log(match)
     const [state, setState] = useState([])
+    const [LoaderState, setLoader]=useState(false)
     useEffect(()=>{
+      setLoader(true)
         fetch(`https://jsonplaceholder.typicode.com/posts/${match.params.id}`,{
             method: 'GET',
             headers: {
@@ -12,15 +16,38 @@ function HomeItem({match}){
             }
           })
           .then(response => response.json())
-          .then(json => setState(json))
+          .then(json => {
+            setState(json)
+            setLoader(false)
+          })
           .catch(err=> console.log(err))
     },[])
     return (
-        <div>
-           <div>{state.id}</div>
-           <div>{state.title}</div>
-           <div>{state.body}</div>
+     <div>
+        <div class="row">
+          
+          <div>
+        <div className='LoaderPosition'>
+         <Loader visible = {LoaderState}
+         type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={300000} //3 secs
+       />
+       </div>{!LoaderState ?
+        <div class="column">
+          <div class="card">
+            <h3>{state.id}</h3>
+            <p>{state.title}</p>
+            <p>{state.body}</p>
+          </div>
+        </div> : ''
+        }
         </div>
+      </div>
+      </div>
+          
     )
 }
 
